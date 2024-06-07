16e20e90ba562ec5b867c0fe3fff9bdc8ce03044
Vivienne Drongowski <vividrongowski@gmail.com>
2024-06-07 16:50:52 +0200

    Added: nugetForUnity and Newtonsoft package

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json; // To install the package, install NuGetForUnity Unity Package Manager and then install Newtonsoft.Json package from NuGetForUnity
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
