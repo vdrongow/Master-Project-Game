733fce2ffd52e640c6261c11f46a0ebe9fc27e43
Vivienne Drongowski <vividrongowski@gmail.com>
2024-06-15 15:44:23 +0200

    Added: ArraySettings and GameSettings ScriptableObjects

---
+++ Assets/Scripts/Configs/ArraySettings.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Configs
+{
+    [CreateAssetMenu(fileName = "ArraySettings", menuName = "Configs/ArraySettings", order = 2)]
+    public sealed class ArraySettings : ScriptableObject
+    {
+        public GameObject barPrefab = null!;
+        
+        public int minValue = 1;
+        public int maxValue = 10;
+        
+        public int minArraySize = 5;
+        public int maxArraySize = 20;
+        
+        // TODO: bool for unique values
+
+        public Color highlightColor = Color.blue;
+        public Color defaultColor = Color.white;
+        public Color sortedColor = Color.grey;
+    }
+}
---
+++ Assets/Scripts/Configs/GameSettings.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Configs
+{
+    [CreateAssetMenu(fileName = "GameSettings", menuName = "Configs/GameSettings", order = 1)]
+    public class GameSettings : ScriptableObject
+    {
+        public string[] sceneNames =
+        {
+            "MainMenu",
+            "Level",
+        };
+    }
+}
