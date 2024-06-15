0215a39ab55a0672ef04a254be32f23d960b2022
Vivienne Drongowski <vividrongowski@gmail.com>
2024-06-15 15:46:28 +0200

    Added: Custom Slider with text

---
+++ Assets/Scripts/MySlider.cs
@@ -0,0 +1,48 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MySlider : MonoBehaviour
+{
+    private Slider slider;
+    [SerializeField]
+    private TextMeshProUGUI handleText;
+    [SerializeField]
+    private TextMeshProUGUI minText;
+    [SerializeField]
+    private TextMeshProUGUI maxText;
+    
+    private void Start()
+    {
+        slider = GetComponent<Slider>();
+        slider.onValueChanged.AddListener(OnSliderValueChanged);
+    }
+    
+    private void OnSliderValueChanged(float value)
+    {
+        handleText.text = value.ToString();
+    }
+    
+    public void SetMinValue(float value)
+    {
+        slider.minValue = value;
+        minText.text = value.ToString();
+    }
+    
+    public void SetMaxValue(float value)
+    {
+        slider.maxValue = value;
+        maxText.text = value.ToString();
+    }
+    
+    public void SetValue(float value)
+    {
+        slider.value = value;
+        handleText.text = value.ToString();
+    }
+    
+    public float GetValue()
+    {
+        return slider.value;
+    }
+}
