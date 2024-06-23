ff7917e188f7b1266d2cc874a75c87b4ec27a105
Vivienne Drongowski <vividrongowski@gmail.com>
2024-06-23 12:51:00 +0200

    Changed: GameStateViewer Text Display

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -1,76 +1,92 @@
 ﻿using System.Linq;
 using System.Text;
 using Enums;
+using Manager;
 using UnityEngine;
 
 public sealed class GameState : MonoBehaviour
 {
-        #region Dump
+    #region Dump
 
-        public string DumpGameState()
+    public string DumpGameState()
+    {
+        var gameManager = GameManager.Singleton;
+        const string empty = "-- Empty --";
+
+        var sb = new StringBuilder();
+        sb.AppendLine("GameState:");
+        sb.AppendLine("-----------------------------");
+
+        // try find Started Sorting Game
+        if (gameManager.SortingGame.IsRunning)
         {
-                var gameManager = GameManager.Singleton;
-                const string empty  = "-- Empty --";
+            sb.AppendLine();
+            sb.AppendLine("Sorting Game:");
+            sb.AppendLine("-----------------------------");
+            sb.AppendLine(gameManager.SortingGame.ToString());
+            PrintSortingGameData();
+        }
+        else
+        {
+            sb.AppendLine(empty);
+            sb.AppendLine();
+        }
 
-                var sb = new StringBuilder();
-                sb.AppendLine("GameState:");
-                sb.AppendLine("-----------------------------");
-                
-                // try find Started Game
-                if(gameManager.Game != null)
+        // try find Started Basic Game
+        if (gameManager.BasicGame.IsRunning)
+        {
+            sb.AppendLine();
+            sb.AppendLine("Basic Game:");
+            sb.AppendLine("-----------------------------");
+            sb.AppendLine(gameManager.BasicGame.ToString());
+        }
+        else
+        {
+            sb.AppendLine(empty);
+        }
+
+        return sb.ToString();
+        
+        void PrintSortingGameData()
+        {
+            // try find ArrayView
+            var levelManager = FindObjectOfType<LevelSortingManager>();
+            if (levelManager != null)
+            {
+                if (levelManager.ArrayView != null)
                 {
-                        sb.AppendLine();
-                        sb.AppendLine("Game:");
-                        sb.AppendLine("-----------------------------");
-                        sb.AppendLine($"SortingAlgorithm: {gameManager.Game.SortingAlgorithm.AsString()}");
-                        sb.AppendLine($"SortType: {gameManager.Game.SortType.AsString()}");
-                        sb.AppendLine($"ArraySize: {gameManager.Game.ArraySize}");
+                    sb.AppendLine();
+                    sb.AppendLine("Array:");
+                    sb.AppendLine("-----------------------------");
+                    sb.AppendLine(levelManager.ArrayView.IsEmpty
+                        ? empty
+                        : string.Join(", \n",
+                            levelManager.ArrayView.ArrayElements.Select((arrayEl, index) =>
+                                $"[{index.ToString()}] {arrayEl}")));
                 }
                 else
                 {
-                        sb.AppendLine(empty);
-                        sb.AppendLine();
-                }
-                
-                // try find ArrayView
-                var levelManager = FindObjectOfType<LevelSortingManager>();
-                if (levelManager != null)
-                {
-                        if(levelManager.ArrayView != null)
-                        {
-                                sb.AppendLine();
-                                sb.AppendLine("Array:");
-                                sb.AppendLine("-----------------------------");
-                                sb.AppendLine();
-                                sb.AppendLine($"{levelManager.ArrayView.SortType.AsString()} Array with {levelManager.ArrayView.ArraySize} elements:");
-                                sb.AppendLine(levelManager.ArrayView.IsEmpty
-                                        ? empty
-                                        : string.Join(", \n", levelManager.ArrayView.ArrayElements.Select((arrayEl, index) => $"[{index.ToString()}] {arrayEl}")));
-                                sb.AppendLine($"Using {levelManager.SortingAlgorithm.GetSortingAlgorithm().AsString()} algorithm");
-                        }
-                        else
-                        {
-                                sb.AppendLine(empty);
-                                sb.AppendLine();
-                        }
-                        if(levelManager.SortingAlgorithm != null)
-                        {
-                                sb.AppendLine();
-                                sb.AppendLine("Sorting Algorithm Steps:");
-                                sb.AppendLine(levelManager.SortingAlgorithm.GetCurrentStepAsString());
-                                sb.AppendLine("-----------------------------");
-                                sb.AppendLine(levelManager.SortingAlgorithm.GetStepsAsString());
-                                sb.AppendLine();
-                        }
-                        else
-                        {
-                                sb.AppendLine(empty);
-                                sb.AppendLine();
-                        }
+                    sb.AppendLine(empty);
+                    sb.AppendLine();
                 }
 
-                return sb.ToString();
+                if (levelManager.SortingAlgorithm != null)
+                {
+                    sb.AppendLine();
+                    sb.AppendLine("Sorting Algorithm Steps:");
+                    sb.AppendLine(levelManager.SortingAlgorithm.GetCurrentStepAsString());
+                    sb.AppendLine("-----------------------------");
+                    sb.AppendLine(levelManager.SortingAlgorithm.GetStepsAsString());
+                    sb.AppendLine();
+                }
+                else
+                {
+                    sb.AppendLine(empty);
+                    sb.AppendLine();
+                }
+            }
         }
-        
-        #endregion
+    }
+
+    #endregion
 }
